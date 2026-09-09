21d754a42867c1363e56493cf1f0ee4103ea72c4
frankie <31193982+frankaroon@users.noreply.github.com>
2026-09-09 17:32:55 -0400

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        // max heap
+        Queue<Integer> maxHeap = new PriorityQueue<>((a,b) -> Integer.compare(b, a));
+        Arrays.stream(stones).forEach(maxHeap::add);
+        // loop ----
+        while (maxHeap.size() > 1) {
+        System.out.println(maxHeap.toString()); 
+        // grab two largest stones
+        int firstStone = maxHeap.poll();
+        int secondStone = maxHeap.poll();
+        // destroy the stones
+        int remainingStone = Math.abs(firstStone - secondStone);
+        // reinclude stone into the simulation
+        maxHeap.add(remainingStone);
+        }
+        // return anything remaining on the heap if none 0
+        return !maxHeap.isEmpty() ? maxHeap.poll() : 0;
+    }
+}
